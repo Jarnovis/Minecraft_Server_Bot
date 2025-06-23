b00f17e56554ecc846a93136d307dd4b4f32ebb0
Your Name <jd.vis@hotmail.nl>
2025-06-23 22:50:20 +0200

    Mods

--- obj/Debug/net8.0/Minecraft_Server_Bot.AssemblyInfo.cs
+++ obj/Debug/net8.0/Minecraft_Server_Bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minecraft_Server_Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f84ebadd803edaa75e8fb164c8562e71a668796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eff75c4c5c49b7cddb9c8c051908abce764c9c58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minecraft_Server_Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minecraft_Server_Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
